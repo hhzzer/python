11df2a2c6e637c03807110c1da0474c7b0c79166
csdnmy <csdnmy@qq.com>
2015-10-28 23:04:35 +0800

    new ex10.py

---
+++ python_code/ex10.py
@@ -0,0 +1,16 @@
+#ex10.py
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
